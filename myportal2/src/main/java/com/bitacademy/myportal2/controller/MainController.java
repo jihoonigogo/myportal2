e082b6a32e963932a4467b9e46d21079635f2291
안지훈 <tw224@naver.com>
2021-04-13 17:32:59 +0900

    파라미터 제거 PascalCase

--- myportal2/src/main/java/com/bitacademy/myportal2/controller/MainController.java
+++ myportal2/src/main/java/com/bitacademy/myportal2/controller/MainController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class Maincontroller {
+public class MainController {
 
 	
 	
 
 		@RequestMapping("/main")
-		public ModelAndView home (@RequestParam String name) {
+		public ModelAndView home () {
 			ModelAndView mav = new ModelAndView();
 			
-			mav.addObject("message","반갑습니다......"+ name);
-			mav.addObject("spring", name + "....봄이 왔습니다.");
+			//mav.addObject("message","반갑습니다......"+ name); @RequestParam String name
+			//mav.addObject("spring", name + "....봄이 왔습니다.");
 			mav.setViewName("/WEB-INF/views/home.jsp");
 			return mav;
 		}
